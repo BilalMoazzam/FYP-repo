311b2d8efad6fec55a9c45de9cb2f26e23d508e8
BilalMoazzam <126339168+BilalMoazzam@users.noreply.github.com>
2025-06-18 19:59:32 +0500

    Delete routes/admin.js

--- routes/admin.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/users', (req, res) => {
-  const users = [ /* your data */ ];
-  res.json(users);
-});
-
-module.exports = router;
